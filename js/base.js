737e9a31846ba8290add779e7b97743ae87b63b2
GustavoLeao2018 <gustavo.leao.nogueira.2017@gmail.com>
2019-03-01 17:16:06 -0300

    menu basico completado

--- js/base.js
+++ js/base.js
@@ -1,5 +1,21 @@
-$(document).ready(function(){
-   var cortexto = $("nav").css("color");
-   $("nav ul li a").css("color", cortexto);
-   
-});
+$(document).ready(
+   function(){
+      var cortexto = $("nav").css("color");
+      
+      if ($("nav").css("color") == 'rgb(0, 0, 0)'){
+         var corhover = "#fff";   
+      }
+      if ($("nav").css("color") == 'rgb(255, 255, 255)'){
+         var corhover = "#000";
+      }
+
+      $("nav ul li a")
+         .mouseenter(
+            function(){ $(this).css("color", corhover); }
+         )
+         .mouseleave(
+            function(){ $(this).css("color", cortexto); }
+         )
+         .css("color", cortexto);
+   }
+);
